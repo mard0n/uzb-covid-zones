d81966c79d27b96dc35b4b1705429ed7aab9e4ff
Palani Kamaraj <palanik@mashreq.com>
2020-02-26 12:18:14 +0400

    Commented fixed layout above certain px

---
+++ src/features/globalCss/GlobalCSS.tsx
@@ -0,0 +1,47 @@
+import { withStyles } from "@mashreq-digital/ui";
+
+const GlobalCss = withStyles(({ palette, spacing }) => ({
+  "@global": {
+    "*, *::before, *::after": {
+      margin: 0,
+      padding: 0
+    },
+    "input:-internal-autofill-selected": {
+      backgroundColor: "transparent !important",
+      color: "inherit !important"
+    },
+    "ul.inline": {
+      display: "flex",
+      justifyContent: "center",
+      "&>li": {
+        padding: `0 ${spacing(2)}px`,
+        "&:last-child": {
+          paddingRight: 0
+        },
+        "&:first-child": {
+          paddingLeft: 0
+        }
+      },
+      "&.border": {
+        "&>li": {
+          borderRight: `1px solid ${palette?.grey?.[500]}`,
+          "&:last-child": {
+            borderRight: "none"
+          }
+        }
+      }
+    },
+    "ul.list-style-none": {
+      listStyleType: "none"
+    },
+    body: {
+      backgroundColor: "#ffffff"
+      // "@media (min-width: 1440px)": {
+      //   maxWidth: "1440px",
+      //   margin: "0 auto"
+      // }
+    }
+  }
+}))(() => null);
+
+export default GlobalCss;
