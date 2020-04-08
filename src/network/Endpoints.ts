d75f0c974e948429ae445755ad60d6a99725d30f
designer63 <hasneetn@mashreq.com>
2020-04-08 15:44:58 +0400

    udpated the end points

--- src/network/Endpoints.ts
+++ src/network/Endpoints.ts
@@ -7,7 +7,7 @@ export const TEST = "/test";
 /* Beneficiary */
 // const Beneficiary_bill_payment
 export const BENEFICIARY_SERVICE_TYPES_ENDPOINT = '/beneficiary-core-service/v1/beneficiary/service-type?category=biller'; //?category="biller" / "utility"
-export const MY_BILL_PAYMENT_BENEFICIARES_ENDPOINT = '/beneficiary-core-service/v1/beneficiary/g    '; //?size=20
+export const MY_BILL_PAYMENT_BENEFICIARES_ENDPOINT = '/beneficiary-core-service/v1/beneficiary/grouped?size=200    '; //?size=20
 export const BILL_PAYMENT_DETECTION_ENDPOINT = `/beneficiary-core-service/v1/beneficiary/${'beneficiaryId'}`;
 export const DELETE_BENEFICIARY_BY_ID_ENDPOINT = `/beneficiary-core-service/v1/beneficiary/${'beneficiaryId'}`;
 export const BILL_PAYMENT_ADD_EDIT_BENEFICIARY_ENDPOINT = `/beneficiary-core-service/v1/beneficiary`;
