b4b930e7f0c2a4c15a02a15264cc02594612d8f6
m.masoodh <m.masoodh@mashreq.com>
2020-04-09 11:37:52 +0400

    salik form

---
+++ src/features/postLogin/billpayment/manage/formData/salik/index.tsx
@@ -0,0 +1,81 @@
+import i18n from "../../../../../../config/i18n";
+import { RegEx } from "../../../../../../util/RegEx";
+import { replaceStr } from "../../../../../../util/helper";
+
+const salik = {
+  type: "salik",
+  fields: {
+    accountNumber: {
+      config: {
+        inputProps: {
+          minLength: 8,
+          maxLength: 12
+        },
+        required: true,
+        value: "",
+        helperText: "common.label.accountNumber",
+        label: "common.label.accountNumber"
+      },
+      validation: {
+        onChangeRegex: RegEx.NUMERIC_ONLY,
+        schema: [
+          {
+            regEx: replaceStr(
+              replaceStr(RegEx.NUMERIC_LIMIT, "min", 8),
+              "max",
+              12
+            ),
+            errorCode: replaceStr(
+              replaceStr(
+                i18n.t("beneficiary.manage.errors.minMaxLength"),
+                "--min--",
+                8
+              ),
+              "--max--",
+              12
+            )
+          }
+        ]
+      },
+      valid: false,
+      touched: false
+    },
+    pincode: {
+      config: {
+        type: 'password',
+        inputProps: {
+          maxLength: 4
+        },
+        required: true,
+        value: "",
+        helperText: "common.label.pincode",
+        label: "SALIK " + i18n.t("common.label.pincode")
+      },
+      validation: {
+        onChangeRegex: RegEx.NUMERIC_ONLY,
+        schema: [
+          {
+            regEx: replaceStr(
+              replaceStr(RegEx.NUMERIC_LIMIT, "min,", ''),
+              "max",
+              4
+            ),
+            errorCode: replaceStr(
+              replaceStr(
+                i18n.t("beneficiary.manage.errors.maxLength"),
+                "--min--",
+                ''
+              ),
+              "--max--",
+              4
+            )
+          }
+        ]
+      },
+      valid: false,
+      touched: false
+    }
+  }
+};
+
+export default salik;
