aa631903fd912310fd14d7a0e6a69f0c277407dc
m.masoodh <m.masoodh@mashreq.com>
2020-04-09 11:49:25 +0400

    utility services setup form cpy

--- src/features/postLogin/billpayment/manage/formData/index.tsx
+++ src/features/postLogin/billpayment/manage/formData/index.tsx
@@ -1,9 +1,18 @@
 import etisalat from "./etisalatFields";
 import du from "./duFields";
 import payAmount from "./payAmountFields";
+import salik from './salik/index';
+import fewa from './utility/fewaFields';
+import noqodi from './utility/noqodiFields';
+import sewa from "./utility/sewaFields";
 
 export const FormFields = {
   etisalat,
   du,
-  payAmount
+  payAmount,
+  salik,
+  fewa,
+  sewa,
+  noqodi
+
 };
