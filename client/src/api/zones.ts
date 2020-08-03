5c0f8a5bf34a3b6fc2419ae9ab234d5f0c1f2b0f
Mardon Mashrabov <mardon.mashrabov@gmail.com>
2020-08-03 21:29:32 +0400

    process.env.PUBLIC_URL test

--- client/src/api/zones.ts
+++ client/src/api/zones.ts
@@ -1,5 +1,6 @@
 import Axios from "axios";
 
 export const fetchZones = () => {
+  console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL);
   return Axios.get(process.env.PUBLIC_URL + "/api/zones");
 };
