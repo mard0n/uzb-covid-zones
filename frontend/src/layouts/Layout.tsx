b7b753b2cbb0f50c5927a3e60e1efc2bd90b1d0c
Mardon <mardon.masharbov@gmail.com>
2022-10-21 18:44:16 +0500

    Layout fixed

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { BottomSheet, BottomSheetRef } from "react-spring-bottom-sheet";
+import React, { useEffect, useRef, useState } from "react";
+import SwipeableBottomSheet from "react-swipeable-bottom-sheet";
 import "react-spring-bottom-sheet/dist/style.css";
 import "./Layout.css";
 
@@ -9,34 +9,43 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ map, body }) => {
-  const sheetRef = useRef<BottomSheetRef>(null);
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+
+  const handleSwipeableChange = (isOpen: boolean) =>
+    setIsBottomSheetOpen(isOpen);
 
   return (
     <>
       <div className="hidden md:flex h-screen w-screen ">
-        <div className="w-[600px] h-full shadow-[0px_4px_40px_rgba(0,30,89,0.09)]">
+        <div className="w-[min(45vw,600px)] z-10 h-full shadow-[0px_4px_40px_rgba(0,30,89,0.09)]">
           {body}
         </div>
         <div className="grow h-full relative">{map}</div>
       </div>
       <div className="block md:hidden h-screen w-screen">
         <div className="h-full w-full">{map}</div>
-        <div className="px-8">
-          <BottomSheet
-            open
-            skipInitialTransition
-            ref={sheetRef}
-            defaultSnap={({ maxHeight }) => maxHeight * 0.5}
-            snapPoints={({ maxHeight }) => [
-              maxHeight * 0.7,
-              maxHeight * 0.5,
-              maxHeight * 0.2,
-            ]}
-            expandOnContentDrag={true}
-          >
+        <SwipeableBottomSheet
+          overflowHeight={200}
+          shadowTip={false}
+          topShadow={false}
+          overlay={false}
+          scrollTopAtClose={true}
+          open={isBottomSheetOpen}
+          onChange={handleSwipeableChange}
+          style={{ zIndex: 3 }}
+          bodyStyle={{
+            borderTopLeftRadius: "1.5rem",
+            borderTopRightRadius: "1.5rem",
+            boxShadow: "0px -10px 40px rgba(0, 30, 89, 0.09)",
+            padding: "32px 20px",
+            backgroundColor: "white",
+          }}
+        >
+          <div className="max-h-[calc(100vh*0.65)] min-h-[136px]">
+            <div className="bottom-sheet-notch" />
             {body}
-          </BottomSheet>
-        </div>
+          </div>
+        </SwipeableBottomSheet>
       </div>
     </>
   );
